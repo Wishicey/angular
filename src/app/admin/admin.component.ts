9e65b106b33225b49452f0d0ba350b42d86c99d1
Bastien <bastien.chanvillard@gmail.com>
2021-01-29 17:18:54 +0100

    modif tab

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -23,15 +23,7 @@ export class AdminComponent implements OnInit {
   }, {
     id: 'name',
     value: 'Name',
-  },
-    {
-      id: 'weight',
-      value: 'Weight',
-    },
-    {
-      id: 'symbol',
-      value: 'Symbol',
-    }];
+  }];
 
   ngOnInit() {
     this.displayedColumns = this.columnNames.map(x => x.id);
@@ -39,12 +31,11 @@ export class AdminComponent implements OnInit {
   }
 
   createTable() {
-    let tableArr: Element[] = [{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-      { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-      { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-      { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-      { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-      { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    let tableArr: Element[] = [{ position: 1, name: 'Polka'},
+      { position: 2, name: 'Menuiserie'},
+      { position: 3, name: 'Portrait'},
+      { position: 4, name: 'Caligraphie'},
+      { position: 5, name: 'Mandarin'},
     ];
     this.dataSource = new MatTableDataSource(tableArr);
     this.dataSource.sort = this.sort;
@@ -54,6 +45,4 @@ export class AdminComponent implements OnInit {
 export interface Element {
   position: number;
   name: string;
-  weight: number;
-  symbol: string;
 }
